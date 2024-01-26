d76318b250dbbd583cbcae430682e4bb3b5b8195
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-01-26 23:37:47 +0530

    Card Settings

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,7 +5,8 @@ import AccordionHeaderSkeleton from "./skeleton/accordion-header";
 import KanbanColumnSkeleton from "./skeleton/kanban";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import DashboardTotalCountCard from "./home/total-count-card";
 
 export {UpcomingEvents , DealsChart , UpcomingEventsSkeleton , AccordionHeaderSkeleton , 
-KanbanColumnSkeleton , ProjectCardSkeleton , LatestActivitiesSkeleton
+KanbanColumnSkeleton , ProjectCardSkeleton , LatestActivitiesSkeleton , DashboardTotalCountCard
 };
