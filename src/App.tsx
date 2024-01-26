cce6073b75eee066448e7bac4ebde599a2b29825
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-01-26 10:51:25 +0530

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import {authProvider , dataProvider , liveProvider} from "./providers"
@@ -6,11 +6,13 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 import {Home , Login , Register , ForgotPassword} from "./pages"
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet , Route, Routes } from "react-router-dom";
+import {Layout} from "../src/components/layout"
 
 function App() {
   return (
@@ -34,11 +36,22 @@ function App() {
                 }}
               >
                 <Routes>
-                  <Route index element={<WelcomePage />} />
-                  <Route index element={<Home />} />
                   <Route path="/register" element={<Register />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="forgot-password" element={<ForgotPassword />} />
+                  <Route 
+                  element = {
+                  <Authenticated
+                    key = "auhenticated-layout"
+                    fallback={<CatchAllNavigate to="/login" />}
+                    >
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>
+                  }>
+                    <Route index element={<Home />} />
+                    </Route>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
