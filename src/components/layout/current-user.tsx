be69b77e94fce66a3a140d35262e698684a14d2e
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-01-26 11:26:04 +0530

    GraphQl and Codegen Setup

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,56 +1,12 @@
 import React from "react";
 
-// import { useGetIdentity } from "@refinedev/core";
-
-// import { SettingOutlined } from "@ant-design/icons";
+import { useGetIdentity } from "@refinedev/core";
 import { Popover } from "antd";
-
-// import type { User } from "@/graphql/schema.types";
-
-// import { CustomAvatar } from "../../custom-avatar";
-// import { Text } from "../../text";
-// import { AccountSettings } from "../account-settings";
+import { CustomAvatar } from "../custom-avatar";
+import type { User } from "@/graphql/schema.types";
 
 export const CurrentUser = () => {
-//   const [opened, setOpened] = React.useState(false);
-//   const { data: user } = useGetIdentity<User>();
-
-//   const content = (
-//     <div
-//       style={{
-//         display: "flex",
-//         flexDirection: "column",
-//       }}
-//     >
-//       <Text
-//         strong
-//         style={{
-//           padding: "12px 20px",
-//         }}
-//       >
-//         {user?.name}
-//       </Text>
-//       <div
-//         style={{
-//           borderTop: "1px solid #d9d9d9",
-//           padding: "4px",
-//           display: "flex",
-//           flexDirection: "column",
-//           gap: "4px",
-//         }}
-//       >
-//         <Button
-//           style={{ textAlign: "left" }}
-//           icon={<SettingOutlined />}
-//           type="text"
-//           block
-//           onClick={() => setOpened(true)}
-//         >
-//           Account settings
-//         </Button>
-//       </div>
-//     </div>
-//   );
+  const { data: user } = useGetIdentity<User>();
 
   return (
     <>
@@ -61,21 +17,13 @@ export const CurrentUser = () => {
         overlayInnerStyle={{ padding: 0 }}
         overlayStyle={{ zIndex: 999 }}
       >
-        Test
-        {/* <CustomAvatar
-          name={user?.name}
-          src={user?.avatarUrl}
+        <CustomAvatar
+        //   name={user?.name}
+        //   src={user?.avatarUrl}
           size="default"
           style={{ cursor: "pointer" }}
-        /> */}
+        /> 
       </Popover>
-      {/* {user && (
-        <AccountSettings
-          opened={opened}
-          setOpened={setOpened}
-          userId={user.id}
-        />
-      )} */}
     </>
   );
 };
