7c8894236e0ac3a6dea5a4731dc2051771d7cdee
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-01-27 15:28:03 +0530

    Initial commit from create-refine-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,54 @@
-import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-import {authProvider , dataProvider , liveProvider} from "./providers"
+
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
-import {Home , Login , Register , ForgotPassword , CompanyList} from "./pages"
+
+import dataProvider, {
+  GraphQLClient,
+  liveProvider,
+} from "@refinedev/nestjs-query";
 import routerBindings, {
-  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { BrowserRouter, Outlet , Route, Routes } from "react-router-dom";
-import {Layout} from "../src/components/layout"
-import { resources } from "./config/resources";
+import { createClient } from "graphql-ws";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { authProvider } from "./authProvider";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+
+const API_URL = "https://api.nestjs-query.refine.dev/graphql";
+const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
+
+const gqlClient = new GraphQLClient(API_URL);
+const wsClient = createClient({ url: WS_URL });
 
 function App() {
   return (
     <BrowserRouter>
       <GitHubBanner />
       <RefineKbarProvider>
+        <ColorModeContextProvider>
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                dataProvider={dataProvider}
-                liveProvider={liveProvider}
+                dataProvider={dataProvider(gqlClient)}
+                liveProvider={liveProvider(wsClient)}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
-                resources={resources}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
                   useNewQueryKeys: true,
-                  projectId: "EdmP7j-vgWQTm-uHcCBv",
+                  projectId: "BjSJl5-YFGDPO-Nn34KJ",
                   liveMode: "auto",
                 }}
               >
                 <Routes>
-                  <Route path="/register" element={<Register />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="forgot-password" element={<ForgotPassword />} />
-                  <Route 
-                  element = {
-                  <Authenticated
-                    key = "auhenticated-layout"
-                    fallback={<CatchAllNavigate to="/login" />}
-                    >
-                      <Layout>
-                        <Outlet />
-                      </Layout>
-                    </Authenticated>
-                  }>
-                    <Route index element={<Home />} />
-                    <Route path= "/companies" element = 
-                    {<CompanyList />} />
-                    </Route>
+                  <Route index element={<WelcomePage />} />
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
@@ -64,6 +57,7 @@ function App() {
               <DevtoolsPanel />
             </DevtoolsProvider>
           </AntdApp>
+        </ColorModeContextProvider>
       </RefineKbarProvider>
     </BrowserRouter>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,1 @@
-import UpcomingEvents from "./home/upcoming-events";
-import DealsChart from "./home/deals-chart";
-import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
-import AccordionHeaderSkeleton from "./skeleton/accordion-header";
-import KanbanColumnSkeleton from "./skeleton/kanban";
-import ProjectCardSkeleton from "./skeleton/project-card";
-import LatestActivitiesSkeleton from "./skeleton/latest-activities";
-import DashboardTotalCountCard from "./home/total-count-card";
-import LatestActivities from "./home/latest-activities";
-
-export {UpcomingEvents , DealsChart , UpcomingEventsSkeleton , AccordionHeaderSkeleton , 
-KanbanColumnSkeleton , ProjectCardSkeleton , LatestActivitiesSkeleton , DashboardTotalCountCard , LatestActivities
-};
+export { Header } from "./header";
