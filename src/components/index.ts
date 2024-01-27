172999effeffd7c300fbef68340c5846393f50e8
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-01-27 19:37:02 +0530

    Done Scene

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,14 +1,8 @@
-import {UpcomingEvents} from "./home/upcoming-events";
-import {DealsChart} from "./home/deals-chart" ;
-import KanbanColumnSkeleton from "./skeleton/kanban";
-import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
-import AccordionHeaderSkeleton from "./skeleton/accordion-header";
-import LatestActivitiesSkeleton from "./skeleton/latest-activities";
-import ProjectCardSkeleton from "./skeleton/project-card";
-import { DashboardTotalCountCard } from "./home/total-count-card";
-import { DashboardLatestActivities } from "./home/latest-activities";
-
-export {UpcomingEvents , DealsChart , KanbanColumnSkeleton , UpcomingEventsSkeleton ,
-    AccordionHeaderSkeleton ,LatestActivitiesSkeleton , ProjectCardSkeleton, DashboardTotalCountCard,
-    DashboardLatestActivities
-}
+export * from "./accordion";
+export * from "./custom-avatar";
+export * from "./icon";
+export * from "./layout";
+export * from "./pagination-total";
+export * from "./select-option-with-avatar";
+export * from "./tags";
+export * from "./text";
