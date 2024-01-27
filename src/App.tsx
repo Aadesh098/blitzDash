6d1ee3ac0eedf1ad72fb6f96575b2c681be78da8
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2024-01-27 16:38:25 +0530

    Phase-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -6,13 +6,14 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 import { dataProvider , liveProvider , authProvider } from "./providers";
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
-import { App as AntdApp } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { App as AntdApp, Layout } from "antd";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { Home , Login , ForgotPassword , Register } from "./pages";
-
+import { resources } from "./config/resources";
 function App() {
   return (
     <BrowserRouter>
@@ -26,6 +27,7 @@ function App() {
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
+                resources={resources}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -35,10 +37,21 @@ function App() {
                 }}
               >
                 <Routes>
-                  <Route index element= {<Home />} />
+                  
                   <Route path="/register" element= {<Register />} />
                   <Route path="/login" element= {<Login />} />
                   <Route path="/forgot-password" element= {<ForgotPassword />} />
+                  <Route element={<Authenticated
+                  key="authenticated-layout"
+                  fallback={<CatchAllNavigate to="/login" />}
+                   > 
+                   <Layout>
+                    <Outlet />
+                  </Layout>
+                   </Authenticated> 
+                  }>
+                   <Route index element= {<Home />} />
+                  </Route>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
